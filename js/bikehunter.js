ba1667e4bea8b81ef3792d0aadca678cccf49f47
samppal <samu.luoma@metropolia.fi>
2021-05-05 17:10:28 +0300

    bug fixxes

--- js/bikehunter.js
+++ js/bikehunter.js
@@ -7,10 +7,9 @@ let markerCollection = {
     "features": []
 };;
 
-let darkTheme = false;
 let map = undefined;
-function switchThemes(){
-if(darkTheme === true){
+function switchThemes(theme){
+if(theme === "dark"){
 map = new mapboxgl.Map({
     container: 'map',
     style: 'https://tiles.stadiamaps.com/styles/alidade_smooth_dark.json',  // Style URL; see our documentation for more options
@@ -19,7 +18,7 @@ map = new mapboxgl.Map({
 });
     map.addControl(new mapboxgl.NavigationControl());
     drawMarkers()
-}else if(darkTheme === false){
+}else if(theme === "light"){
     map = new mapboxgl.Map({
         container: 'map',
         style: 'https://tiles.stadiamaps.com/styles/alidade_smooth.json',  // Style URL; see our documentation for more options
